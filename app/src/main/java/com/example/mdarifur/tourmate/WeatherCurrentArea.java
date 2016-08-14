1f4c2fb5960b037bab1ac6fba421ca51ec8e26ad
Arif <iarif4u@gmail.com>
2016-08-15 02:40:37 +0600

    Just start

---
+++ app/src/main/java/com/example/mdarifur/tourmate/WeatherCurrentArea.java
@@ -0,0 +1,14 @@
+package com.example.mdarifur.tourmate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherCurrentArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_current_area);
+
+    }
+}
