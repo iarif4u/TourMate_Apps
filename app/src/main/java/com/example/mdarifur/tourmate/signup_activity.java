bd6b7f4b01d6d161668e772a31766a79dd10e153
Arif <iarif4u@gmail.com>
2016-08-15 23:15:56 +0600

    File Streaming Success

--- app/src/main/java/com/example/mdarifur/tourmate/signup_activity.java
+++ app/src/main/java/com/example/mdarifur/tourmate/signup_activity.java
@@ -2,26 +2,21 @@ package com.example.mdarifur.tourmate;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import com.example.mdarifur.tourmate.FileOperation.FileSystem;
 
 public class Signup_activity extends AppCompatActivity {
+    private FileSystem fileSystem;
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    String mCurrentPhotoPath;
     ImageView cameraIV;
-
+    Bitmap profileIMG = null;
+    String imageName = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,42 +24,26 @@ public class Signup_activity extends AppCompatActivity {
     }
 
     public void ImageCapture(View view) {
+        fileSystem = new FileSystem(this);
+
         cameraIV = (ImageView) findViewById(R.id.cameraIV);
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
-
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            cameraIV.setImageBitmap(imageBitmap);
+            profileIMG = (Bitmap) extras.get("data");
+            imageName = fileSystem.saveToInternalStorage(profileIMG);
         }
     }
-
-    private File createImageFile(){
-            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            String imageFileName = "JPEG_" + timeStamp + "_";
-            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = null;
-        try {
-            image = File.createTempFile(
-                    imageFileName,  /* prefix */
-                    ".jpg",         /* suffix */
-                    storageDir      /* directory */
-            );
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Save a file: path for use with ACTION_VIEW intents
-            mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-            return image;
-        }
-
+    public void SaveData(View view) {
+        Bitmap b = fileSystem.loadImageFromStorage(imageName);
+        Toast.makeText(Signup_activity.this, imageName, Toast.LENGTH_SHORT).show();
+        cameraIV.setImageBitmap(b);
+    }
 }
