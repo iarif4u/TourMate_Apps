7ff24e9fc00282025239a4b0c56098f7dbef052b
Arif <iarif4u@gmail.com>
2016-08-15 16:22:37 +0600

    Splash Activity Done

--- app/src/main/java/com/example/mdarifur/tourmate/MainActivity.java
+++ app/src/main/java/com/example/mdarifur/tourmate/MainActivity.java
@@ -81,14 +81,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.weather) {
             Intent intent = new Intent(this,WeatherCurrentArea.class);
             startActivity(intent);
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
